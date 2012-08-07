d73d49640e167916ad038aa6f7695ab134de763a
lucien <zurcherl@W538.berufzug.ch>
2012-08-07 16:25:19 +0200

    Initial Commit$

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: zurcherl
+ *
+ * Created on 7. August 2012, 16:16
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return 0;
+}
+
